f095829fa0387d99dcfa93793fa8ad841919db33
Ju1-js <40339350+Ju1-js@users.noreply.github.com>
2023-07-12 15:18:03 -0700

    Update discord.js

--- api/discord.js
+++ api/discord.js
@@ -1,9 +1,11 @@
 module.exports = (req, res) => {
   const userAgent = req.headers["user-agent"];
+  console.log("User-Agent:", userAgent);
+
   const isDiscordBot = userAgent.includes("Discordbot");
 
   if (isDiscordBot) {
-    res.redirect("/img/middleclickforcalc.png");
+    res.redirect("middleclickforcalc.png");
   } else {
     res.redirect("/api/ip");
   }

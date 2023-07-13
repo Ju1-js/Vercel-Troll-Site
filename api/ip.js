2e8ec1147fa2c1f9cf995f0101880c8899bf8072
Ju1-js <40339350+Ju1-js@users.noreply.github.com>
2023-07-13 15:13:56 -0700

    Update ip.js

--- api/ip.js
+++ api/ip.js
@@ -24,7 +24,7 @@ module.exports = (req, res) => {
             left: 0;
             width: 100%;
             height: 100%;
-            background-color: rgba(0, 0, 0, 0.8);
+            background-color: rgba(0, 0, 0, 0);
             color: white;
             display: flex;
             align-items: center;

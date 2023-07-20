c90e3fb5761d3c8c65f26f6edc159f6b56757c7e
Ju1-js <40339350+Ju1-js@users.noreply.github.com>
2023-07-20 14:21:04 -0700

    Update ip.js

--- api/ip.js
+++ api/ip.js
@@ -2,10 +2,12 @@ const fs = require("fs").promises;
 const path = require("path");
 module.exports = async (req, res) => {
   try {
+    const html = await fs.readFile(
+      path.join(process.cwd(), "html", "ip.html"),
+      "utf8"
+    );
     const ip = req.headers["x-real-ip"] || req.connection.remoteAddress;
-    const html = await fs
-      .readFile(path.join(process.cwd(), "html", "ip.html"), "utf8")
-      .replace("${ip}", ip);
+    html = html.replace("${ip}", ip);
     res.setHeader("Content-Type", "text/html");
     res.status(200).send(html);
   } catch (e) {

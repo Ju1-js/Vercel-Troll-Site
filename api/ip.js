8080d068079139f240ab8397761f9c27707d8fbf
Ju1-js <40339350+Ju1-js@users.noreply.github.com>
2023-07-20 14:09:08 -0700

    Forgot important import 💀

--- api/ip.js
+++ api/ip.js
@@ -1,7 +1,8 @@
+import { promises as fs } from "fs";
 module.exports = async (req, res) => {
   const ip = req.headers["x-real-ip"] || req.connection.remoteAddress;
   try {
-    const html = await readFile("../html/ip.html", "utf8");
+    const html = await fs.readFile("../html/ip.html", "utf8");
     res.setHeader("Content-Type", "text/html");
     res.status(200).send(html);
   } catch (e) {

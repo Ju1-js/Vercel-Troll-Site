6dd33588b06f2d620e623107c5071e6c49fb23b9
Ju1-js <40339350+Ju1-js@users.noreply.github.com>
2023-07-20 14:18:23 -0700

    Re-add functionality

--- api/ip.js
+++ api/ip.js
@@ -1,16 +1,15 @@
 const fs = require("fs").promises;
 const path = require("path");
 module.exports = async (req, res) => {
-  const ip = req.headers["x-real-ip"] || req.connection.remoteAddress;
   try {
-    const html = await fs.readFile(
-      path.join(process.cwd(), "html", "ip.html"),
-      "utf8"
-    );
+    const ip = req.headers["x-real-ip"] || req.connection.remoteAddress;
+    const html = await fs
+      .readFile(path.join(process.cwd(), "html", "ip.html"), "utf8")
+      .replace("${ip}", ip);
     res.setHeader("Content-Type", "text/html");
     res.status(200).send(html);
   } catch (e) {
-    console.error("Error reading file:", e);
+    console.error(e);
     res.status(500).send("Internal Server Error");
   }
 };

d08711e436d947846ee3c64b095d8ae29facf918
Ju1-js <40339350+Ju1-js@users.noreply.github.com>
2023-07-20 14:06:28 -0700

    Update ip.js

--- api/ip.js
+++ api/ip.js
@@ -1,9 +1,11 @@
-module.exports = (req, res) => {
+module.exports = async (req, res) => {
   const ip = req.headers["x-real-ip"] || req.connection.remoteAddress;
-  const html = fs.readFileSync(
-    path.join(__dirname, "..", "html", "ip.html"),
-    "utf8"
-  );
-  res.setHeader("Content-Type", "text/html");
-  res.status(200).send(html);
+  try {
+    const html = await readFile("../html/ip.html", "utf8");
+    res.setHeader("Content-Type", "text/html");
+    res.status(200).send(html);
+  } catch (e) {
+    console.error("Error reading file:", e);
+    res.status(500).send("Internal Server Error");
+  }
 };
